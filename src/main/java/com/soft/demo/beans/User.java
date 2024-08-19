b0be4f6e771e36c32fa54b9190af1fb4cf05e71a
aolingge <1930668092@qq.com>
2024-08-19 14:43:10 +0800

    更改

---
+++ src/main/java/com/soft/demo/beans/User.java
@@ -0,0 +1,14 @@
+package com.soft.demo.beans;
+
+public class User {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private  String name;
+
+}
