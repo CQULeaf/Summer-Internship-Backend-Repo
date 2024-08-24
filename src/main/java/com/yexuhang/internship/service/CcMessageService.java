2cadd42f5a329011afd1aac35f01e8eff0634d35
Xuhang Ye <609388896@qq.com>
2024-08-24 19:35:43 +0800

    use CodeGenerator to generate message part.

---
+++ src/main/java/com/yexuhang/internship/service/CcMessageService.java
@@ -0,0 +1,16 @@
+package com.yexuhang.internship.service;
+
+import com.yexuhang.internship.bean.CcMessage;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 消息表 服务类
+ * </p>
+ *
+ * @author Xuhang Ye
+ * @since 2024-08-24
+ */
+public interface CcMessageService extends IService<CcMessage> {
+
+}
