e5f9944afa2b9d2ae68458b06635a5d0a76dffde
aolingge <1930668092@qq.com>
2024-08-24 17:55:51 +0800

    修改用户好友接口，和获取用户关注的超话及其详细信息的接口

---
+++ src/main/java/com/yexuhang/internship/mapper/CcTopicMapper.java
@@ -0,0 +1,18 @@
+package com.yexuhang.internship.mapper;
+
+import com.yexuhang.internship.bean.CcTopic;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 话题表 Mapper 接口
+ * </p>
+ *
+ * @author Xuhang Ye
+ * @since 2024-08-24
+ */
+@Mapper
+public interface CcTopicMapper extends BaseMapper<CcTopic> {
+
+}
