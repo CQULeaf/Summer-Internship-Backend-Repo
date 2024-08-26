95c892b5839ac8588a88dbd865d405d0e4041bb1
Xuhang Ye <609388896@qq.com>
2024-08-26 09:24:44 +0800

    merge at 8/26

--- src/main/java/com/yexuhang/internship/config/WebSocketConfig.java
+++ src/main/java/com/yexuhang/internship/config/WebSocketConfig.java
@@ -1,19 +1,45 @@
 package com.yexuhang.internship.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 
 /**
  * @author Xuhang Ye
- * @time 12:06 AM
  */
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-        @Override
-        public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-            registry.addHandler(new ChatWebSocketHandler(), "/chat").setAllowedOrigins("*");
-        }
+//注解用于开启使用STOMP协议来传输基于代理（MessageBroker）的消息，这时候控制器（controller）开始支持@MessageMapping,就像是使用@requestMapping一样。
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    /**
+     * 注册端点，发布或者订阅消息的时候需要连接此端点
+     * setAllowedOrigins 非必须，*表示允许其他域进行连接
+     * withSockJS  表示开始sockejs支持
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        registry.addEndpoint("/endpoint-websocket")
+                .setAllowedOrigins("*")
+                .withSockJS()
+                .setHeartbeatTime(10000);
+    }
+
+    /**
+     * 配置消息代理(中介)
+     * enableSimpleBroker 服务端推送给客户端的路径前缀
+     * setApplicationDestinationPrefixes  客户端发送数据给服务器端的一个前缀
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.enableSimpleBroker("/topic", "/chat")
+                .setHeartbeatValue(new long[] {10000, 10000})
+                .setTaskScheduler(new DefaultManagedTaskScheduler());
+        registry.setApplicationDestinationPrefixes("/app");
+    }
 }
